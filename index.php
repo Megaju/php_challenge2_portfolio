f4e39cd90ce3c380bd69ae273f3ef12ff19c0a33
Megaju <julen_malle@outlook.fr>
2016-09-04 20:20:38 +0200

    tiles modifiés

--- index.php
+++ index.php
@@ -12,26 +12,30 @@
         <div class="carousel"></div>
         <section>
             <h2>Projets</h2>
-            <div class="grid bloc-tiles">
+            <div class="grid">
                 <figure class="effect-apollo"> <img src="img/img-test-1.jpg" alt="" />
                     <figcaption>
                         <h2>Titre de test<span>Bold</span></h2>
-                        <p>Description rapide du tile...</p> <a href="#">La suite...</a> </figcaption>
+                        <p>Description rapide du tile...</p>
+                    </figcaption>
                 </figure>
                 <figure class="effect-apollo"> <img src="img/img-test-2.jpg" alt="" />
                     <figcaption>
                         <h2>Titre de test<span>Bold</span></h2>
-                        <p>Description rapide du tile...</p> <a href="#">La suite...</a> </figcaption>
+                        <p>Description rapide du tile...</p>
+                    </figcaption>
                 </figure>
                 <figure class="effect-apollo"> <img src="img/img-test-3.jpg" alt="" />
                     <figcaption>
                         <h2>Titre de test<span>Bold</span></h2>
-                        <p>Description rapide du tile...</p> <a href="#">La suite...</a> </figcaption>
+                        <p>Description rapide du tile...</p>
+                    </figcaption>
                 </figure>
                 <figure class="effect-apollo"> <img src="img/img-test-4.jpg" alt="" />
                     <figcaption>
                         <h2>Titre de test<span>Bold</span></h2>
-                        <p>Description rapide du tile...</p> <a href="#">La suite...</a> </figcaption>
+                        <p>Description rapide du tile...</p>
+                    </figcaption>
                 </figure>
             </div>
         </section>
