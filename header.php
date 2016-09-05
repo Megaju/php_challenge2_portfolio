b2ca25807b3e9be5b6dd02685f74cd55c390e245
Megaju <julen_malle@outlook.fr>
2016-09-05 23:12:06 +0200

    nouvelle fonts

--- header.php
+++ header.php
@@ -6,8 +6,10 @@
     <title>JULIEN MALLE - Portfolio</title>
     <link rel="stylesheet" href="css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lobster|Poiret+One" rel="stylesheet"> 
     <link rel="stylesheet" href="css/slideshow.css">
     <link rel="stylesheet" href="css/page.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
     <script src="js/modernizr.custom.js"></script>
 </head>
 
@@ -16,10 +18,10 @@
         <h1><a href="#">Julien Malle <span>- Portfolio</span></a></h1>
         <nav class="top">
             <ul>
-                <li><a href="#">CV</a></li>
-                <li><a href="#">Portfolio</a></li>
-                <li><a href="#">Github</a></li>
-                <li><a href="#">Linkedin</a></li>
+                <li><a href="#"><i class="fa fa-file-text"></i> CV</a></li>
+                <li><a href="#"><i class="fa fa-th-large"></i> Portfolio</a></li>
+                <li><a href="#"><i class="fa fa-github"></i> Github</a></li>
+                <li><a href="#"><i class="fa fa-linkedin-square"></i> Linkedin</a></li>
             </ul>
         </nav>
     </header>
