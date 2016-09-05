5b609b788dd630f08f5519ddc19ed88431f98349
Megaju <julen_malle@outlook.fr>
2016-09-05 21:05:46 +0200

    ajout page.php

--- header.php
+++ header.php
@@ -1,11 +1,24 @@
-<header>
-    <h1><a href="#">Julien Malle <span>- Portfolio</span></a></h1>
-    <nav class="top">
-        <ul>
-            <li><a href="#">CV</a></li>
-            <li><a href="#">Portfolio</a></li>
-            <li><a href="#">Github</a></li>
-            <li><a href="#">Linkedin</a></li>
-        </ul>
-    </nav>
-</header>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>JULIEN MALLE - Portfolio</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="css/slideshow.css">
+    <script src="js/modernizr.custom.js"></script>
+</head>
+
+<body>
+    <header>
+        <h1><a href="#">Julien Malle <span>- Portfolio</span></a></h1>
+        <nav class="top">
+            <ul>
+                <li><a href="#">CV</a></li>
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">Github</a></li>
+                <li><a href="#">Linkedin</a></li>
+            </ul>
+        </nav>
+    </header>
