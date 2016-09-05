9ec1c588a5f835ec57ddb6ee262601c4beed9b1f
Megaju <julen_malle@outlook.fr>
2016-09-05 20:26:12 +0200

    integration slide + qq modif - non fini

--- index.php
+++ index.php
@@ -5,66 +5,70 @@
     <meta charset="UTF-8">
     <title>JULIEN MALLE - Portfolio</title>
     <link rel="stylesheet" href="css/style.css">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"> </head>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="css/slideshow.css">
+    <script src="js/modernizr.custom.js"></script>
+</head>
 
 <body>
     <?php include('header.php'); ?>
-        <div class="carousel"></div>
-        <section>
-            <h2>Projets</h2>
-            <div class="grid"><a href="#">
-                <figure class="tile-effect"> <img src="img/img-test-1.jpg" alt="" />
-                    <figcaption>
-                        <h2>Titre de test<span>Bold</span></h2>
-                        <p>Description rapide du tile...</p>
-                    </figcaption>
-                </a></figure>
-                <figure class="tile-effect"> <img src="img/img-test-2.jpg" alt="" />
-                    <figcaption>
-                        <h2>Titre de test<span>Bold</span></h2>
-                        <p>Description rapide du tile...</p>
-                    </figcaption>
-                </figure>
-                <figure class="tile-effect"> <img src="img/img-test-3.jpg" alt="" />
-                    <figcaption>
-                        <h2>Titre de test<span>Bold</span></h2>
-                        <p>Description rapide du tile...</p>
-                    </figcaption>
-                </figure>
-                <figure class="tile-effect"> <img src="img/img-test-4.jpg" alt="" />
-                    <figcaption>
-                        <h2>Titre de test<span>Bold</span></h2>
-                        <p>Description rapide du tile...</p>
-                    </figcaption>
-                </figure>
-                
-                <figure class="tile-effect"> <img src="img/img-test-1.jpg" alt="" />
-                    <figcaption>
-                        <h2>Titre de test<span>Bold</span></h2>
-                        <p>Description rapide du tile...</p>
-                    </figcaption>
-                </figure>
-                <figure class="tile-effect"> <img src="img/img-test-2.jpg" alt="" />
-                    <figcaption>
-                        <h2>Titre de test<span>Bold</span></h2>
-                        <p>Description rapide du tile...</p>
-                    </figcaption>
-                </figure>
-                <figure class="tile-effect"> <img src="img/img-test-3.jpg" alt="" />
-                    <figcaption>
-                        <h2>Titre de test<span>Bold</span></h2>
-                        <p>Description rapide du tile...</p>
-                    </figcaption>
-                </figure>
-                <figure class="tile-effect"> <img src="img/img-test-4.jpg" alt="" />
-                    <figcaption>
-                        <h2>Titre de test<span>Bold</span></h2>
-                        <p>Description rapide du tile...</p>
-                    </figcaption>
-                </figure>
-            </div>
-        </section>
-        <?php include('footer.php'); ?>
+        <?php include('slideshow.php'); ?>
+            <section>
+                <h2>Projets</h2>
+                <div class="grid">
+                    <a href="#">
+                        <figure class="tile-effect"> <img src="img/img-test-1.jpg" alt="" />
+                            <figcaption>
+                                <h2>Titre de test<span>Bold</span></h2>
+                                <p>Description rapide du tile...</p>
+                            </figcaption>
+                    </a>
+                    </figure>
+                    <figure class="tile-effect"> <img src="img/img-test-2.jpg" alt="" />
+                        <figcaption>
+                            <h2>Titre de test<span>Bold</span></h2>
+                            <p>Description rapide du tile...</p>
+                        </figcaption>
+                    </figure>
+                    <figure class="tile-effect"> <img src="img/img-test-3.jpg" alt="" />
+                        <figcaption>
+                            <h2>Titre de test<span>Bold</span></h2>
+                            <p>Description rapide du tile...</p>
+                        </figcaption>
+                    </figure>
+                    <figure class="tile-effect"> <img src="img/img-test-4.jpg" alt="" />
+                        <figcaption>
+                            <h2>Titre de test<span>Bold</span></h2>
+                            <p>Description rapide du tile...</p>
+                        </figcaption>
+                    </figure>
+                    <figure class="tile-effect"> <img src="img/img-test-1.jpg" alt="" />
+                        <figcaption>
+                            <h2>Titre de test<span>Bold</span></h2>
+                            <p>Description rapide du tile...</p>
+                        </figcaption>
+                    </figure>
+                    <figure class="tile-effect"> <img src="img/img-test-2.jpg" alt="" />
+                        <figcaption>
+                            <h2>Titre de test<span>Bold</span></h2>
+                            <p>Description rapide du tile...</p>
+                        </figcaption>
+                    </figure>
+                    <figure class="tile-effect"> <img src="img/img-test-3.jpg" alt="" />
+                        <figcaption>
+                            <h2>Titre de test<span>Bold</span></h2>
+                            <p>Description rapide du tile...</p>
+                        </figcaption>
+                    </figure>
+                    <figure class="tile-effect"> <img src="img/img-test-4.jpg" alt="" />
+                        <figcaption>
+                            <h2>Titre de test<span>Bold</span></h2>
+                            <p>Description rapide du tile...</p>
+                        </figcaption>
+                    </figure>
+                </div>
+            </section>
+            <?php include('footer.php'); ?>
 </body>
 
 </html>
